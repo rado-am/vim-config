7c6cafa0000466b8076127c8a82b0cb88be6e99c
Radosław Łuczak <radoam@192.168.1.4>
2024-02-01 15:10:45 +0100

    Add select all key bindings in insert mode

--- lua/nanovim/core/keymaps.lua
+++ lua/nanovim/core/keymaps.lua
@@ -2,4 +2,6 @@ local keymap = vim.keymap
 
 vim.g.mapleader = " " -- set leader key to space
 
-keymap.set("n", "<C-a>", "ggVG") -- select all text in file with Ctrl + a
+-- Map Ctrl + a to select all text in normal & insert mode
+keymap.set("n", "<C-a>", "ggVG")
+keymap.set("i", "<C-a>", "<Esc>ggVG")
