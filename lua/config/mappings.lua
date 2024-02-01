46f7bc81bdd2e0121f59b5b76ba8d5339287ef82
rado-am <129002620+rado-am@users.noreply.github.com>
2024-02-01 09:28:18 +0100

    Delete lua directory

--- lua/config/mappings.lua
+++
@@ -1,6 +0,0 @@
-vim.g.mapleader = " "
-
-vim.api.nvim_set_keymap('n', '<leader>a', 'ggVG', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('x', '<leader>c', '"+y', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>z', ':u<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>s', ':w<CR>', { noremap = true, silent = true })
