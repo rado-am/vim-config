bb9e45041f9e7528463cd566486ae5cfc6a3296e
Radosław Łuczak <radoam@192.168.1.4>
2024-02-01 07:28:51 +0100

    Reformat files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,45 +1,45 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        ensure_installed = {
-          "c",
-          "lua",
-          "luadoc",
-          "vim",
-          "vimdoc",
-          "css",
-          "c_sharp",
-          "dockerfile",
-          "elixir",
-          "erlang",
-          "bash",
-          "gitignore",
-          "graphql",
-          "html",
-          "javascript",
-          "json",
-          "prisma",
-          "rust",
-          "scss",
-          "sql",
-          "svelte",
-          "terraform",
-          "tsx",
-          "typescript",
-          "vue",
-          "xml",
-          "yaml",
-          "markdown"
-        },
-        sync_install = false,
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"luadoc",
+					"vim",
+					"vimdoc",
+					"css",
+					"c_sharp",
+					"dockerfile",
+					"elixir",
+					"erlang",
+					"bash",
+					"gitignore",
+					"graphql",
+					"html",
+					"javascript",
+					"json",
+					"prisma",
+					"rust",
+					"scss",
+					"sql",
+					"svelte",
+					"terraform",
+					"tsx",
+					"typescript",
+					"vue",
+					"xml",
+					"yaml",
+					"markdown",
+				},
+				sync_install = false,
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
