f3e3e4e20910040b20ceb1b96d109a6bc67e5145
Radosław Łuczak <radoam@192.168.1.4>
2024-02-01 18:26:53 +0100

    Add base configuration options

--- lua/nanovim/core/options.lua
+++ lua/nanovim/core/options.lua
@@ -1,1 +1,28 @@
-print("Welcome from options")
+local opt = vim.opt
+
+-- Line number
+opt.nu = true
+opt.relativenumber = true
+
+-- Tabs $ indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.expandtab = true
+opt.smartindent = true
+
+-- Line wrapping
+opt.wrap = false
+
+-- Search settings
+opt.ignorecase = true
+opt.smartcase = true -- if you include mixed case in your search, assymes you want case-sensitive
+opt.hlsearch = true
+opt.incsearch = true
+
+-- Apperance
+opt.termguicolors = true
+opt.scrolloff = 10
+
+-- Rest
+opt.updatetime = 50
