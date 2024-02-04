c9764214ff009dac922118b06db51d316266c6eb
Radosław Łuczak <radoam@192.168.1.4>
2024-02-04 08:57:30 +0100

    Add treesitter & languages servers

---
+++ lua/nanovim/plugins/treesitter.lua
@@ -0,0 +1,55 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  dependencies = {
+    "windwp/nvim-ts-autotag"
+  },
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
+
+    treesitter.setup({
+      highlight = { enable = true },
+      indent = { enable = true },
+      autotag = { enable = true },
+      ensure_installed = {
+        "angular",
+        "bash",
+        "c",
+        "c_sharp",
+        "cpp",
+        "css",
+        "dockerfile",
+        "elixir",
+        "erlang",
+        "gitignore",
+        "go",
+        "graphql",
+        "html",
+        "http",
+        "java",
+        "javascript",
+        "jq",
+        "jsdoc",
+        "json",
+        "kotlin",
+        "lua",
+        "luadoc",
+        "markdown",
+        "markdown_inline",
+        "prisma",
+        "regex",
+        "ruby",
+        "rust",
+        "scss",
+        "sql",
+        "styled",
+        "svelte",
+        "terraform",
+        "typescript",
+        "vue",
+        "xml",
+        "yaml",
+      },
+    })
+  end
+}
