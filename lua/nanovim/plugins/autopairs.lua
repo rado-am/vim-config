5853e2d9a445e74390b11071a626321ad2b95ce3
Radosław Łuczak <radoam@192.168.1.4>
2024-02-07 08:22:24 +0100

    Add typescript support

--- lua/nanovim/plugins/autopairs.lua
+++ lua/nanovim/plugins/autopairs.lua
@@ -7,7 +7,8 @@ return {
       check_ts = true,
       ts_config = {
         lua = { "string" },
-        javascript = { "template_string" }
+        javascript = { "template_string", "string" },
+        typescript = { "template_string", "string" },
       }
     })
   end
