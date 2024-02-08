c0fa4d5ceb12954c3fc09a11c2d2540ab0796ebd
Radosław Łuczak <radoam@192.168.1.4>
2024-02-08 09:49:07 +0100

    Disable autopairs in string

--- lua/nanovim/plugins/autopairs.lua
+++ lua/nanovim/plugins/autopairs.lua
@@ -2,17 +2,25 @@ return {
   'windwp/nvim-autopairs',
   event = "InsertEnter",
   config = function()
-    local Rule = require('nvim-autopairs.rule')
     local npairs = require('nvim-autopairs')
+    local Rule = require('nvim-autopairs.rule')
+    local ts_conds = require('nvim-autopairs.ts-conds')
     
     npairs.setup({
       enable_check_bracket_line = true,
       check_ts = true,
       ts_config = {
         lua = { "string" },
-        javascript = { "template_string", "string" },
-        typescript = { "template_string", "string" },
+        javascript = { "string" },
+        typescript = { "string" },
       }
+    })
+
+    -- Disable autopairs inside strings
+    npairs.add_rules({
+      Rule("(", ""):with_pair(ts_conds.is_ts_node({'string'})),
+      Rule("{", ""):with_pair(ts_conds.is_ts_node({'string'})),
+      Rule("[", ""):with_pair(ts_conds.is_ts_node({'string'})),
     })
   end
 }
