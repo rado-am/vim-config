73d6b38a76b30b729ede7f8ae0532bd1ccb9d3d9
Radosław Łuczak <radoam@192.168.1.4>
2024-02-10 11:44:06 +0100

    Add ui seletc for code-actions

--- lua/nanovim/plugins/telescope.lua
+++ lua/nanovim/plugins/telescope.lua
@@ -3,15 +3,26 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    "nvim-telescope/telescope-ui-select.nvim",
   },
   config = function()
     local telescope = require("telescope")
     local builtin = require('telescope.builtin')
+    local themes = require("telescope.themes")
+
+    telescope.setup({
+      extensions = {
+        ["ui-select"] = {
+          themes.get_dropdown({}),
+        },
+      },
+    })
 
     vim.keymap.set('n', '<C-p>', builtin.find_files, {})
     vim.keymap.set('n', '<sc-p>',builtin.live_grep, {})
     vim.keymap.set('n', '<sc-g>', builtin.git_files, {})
 
     telescope.load_extension('fzf')
+    telescope.load_extension('ui-select')
   end
 }
