ab06b2e43b497744b8e07f445d6cee7a2ab79d18
Radosław Łuczak <radoam@Radosaws-MacBook-Pro.local>
2024-02-11 17:38:11 +0100

    Configure format on save

--- lua/nanovim/plugins/lsp/nonels.lua
+++ lua/nanovim/plugins/lsp/nonels.lua
@@ -1,34 +1,47 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		local formatting = null_ls.builtins.formatting
-		local diagnostics = null_ls.builtins.diagnostics
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    local formatting = null_ls.builtins.formatting
+    local diagnostics = null_ls.builtins.diagnostics
 
-		local keymap = vim.keymap
-		null_ls.setup({
-			sources = {
-				formatting.stylua,
-				formatting.prettierd.with({
-					extra_filetypes = { "svelte" }, -- support svelte files
-				}),
-				formatting.rustfmt,
-				formatting.beautysh,
-				formatting.rustywind,
-				formatting.sqlfluff.with({
-					extra_args = { "--dialect", "postgres" }, -- change to your dialect
-				}),
-				diagnostics.eslint_d,
-				diagnostics.vint,
-				diagnostics.hadolint,
-				diagnostics.actionlint,
-				diagnostics.sqlfluff.with({
-					extra_args = { "--dialect", "postgres" }, -- change to your dialect
-				}),
-				diagnostics.luacheck,
-			},
-		})
+    local keymap = vim.keymap
+    null_ls.setup({
+      sources = {
+        formatting.stylua,
+        formatting.prettierd.with({
+          extra_filetypes = { "svelte" }, -- support svelte files
+        }),
+        formatting.rustfmt,
+        formatting.beautysh,
+        formatting.rustywind,
+        formatting.sqlfluff.with({
+          extra_args = { "--dialect", "postgres" }, -- change to your dialect
+        }),
+        diagnostics.eslint_d,
+        diagnostics.vint,
+        diagnostics.hadolint,
+        diagnostics.actionlint,
+        diagnostics.sqlfluff.with({
+          extra_args = { "--dialect", "postgres" }, -- change to your dialect
+        }),
+        diagnostics.luacheck,
+      },
+      -- format on save
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format()
+            end,
+          })
+        end
+      end,
+    })
 
-		keymap.set({ "n", "v", "i" }, "<M-Enter>", vim.lsp.buf.format, {})
-	end,
+    keymap.set({ "n", "v", "i" }, "<M-Enter>", vim.lsp.buf.format, {})
+  end,
 }
