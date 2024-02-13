4ce7e217f1e6684333d28c8ebc288246da307db4
Radosław Łuczak <radoam@Radosaws-MacBook-Pro.local>
2024-02-13 08:19:28 +0100

    Add autocompletion

--- lua/nanovim/plugins/lsp/lspconfig.lua
+++ lua/nanovim/plugins/lsp/lspconfig.lua
@@ -1,31 +1,68 @@
 return {
-  "neovim/nvim-lspconfig",
-  config = function()
-    local lspconfig = require("lspconfig")
-    local keymap = vim.keymap
+	"neovim/nvim-lspconfig",
+	config = function()
+		local lspconfig = require("lspconfig")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local keymap = vim.keymap
 
-    lspconfig.lua_ls.setup({})
-    lspconfig.rust_analyzer.setup({})
-    lspconfig.tsserver.setup({})
-    lspconfig.html.setup({})
-    lspconfig.svelte.setup({})
-    lspconfig.vuels.setup({})
-    lspconfig.cssls.setup({})
-    lspconfig.bashls.setup({})
-    lspconfig.prismals.setup({})
-    lspconfig.vimls.setup({})
-    lspconfig.lemminx.setup({})
-    lspconfig.docker_compose_language_service.setup({})
-    lspconfig.dockerls.setup({})
-    lspconfig.graphql.setup({})
-    lspconfig.tailwindcss.setup({})
-    lspconfig.marksman.setup({})
-    lspconfig.sqls.setup({})
-    lspconfig.yamlls.setup({})
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.rust_analyzer.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.tsserver.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.html.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.svelte.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.vuels.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.cssls.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.bashls.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.prismals.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.vimls.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.lemminx.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.docker_compose_language_service.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.dockerls.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.graphql.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.tailwindcss.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.marksman.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.sqls.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.yamlls.setup({
+			capabilities = capabilities,
+		})
 
-    keymap.set("n", "K", vim.lsp.buf.hover, {})
-    keymap.set("n", "D", vim.lsp.buf.definition, {})
-    keymap.set({ "n", "v", "i" }, "<C-Enter>", vim.lsp.buf.code_action, {})
-    keymap.set("n", "E", vim.diagnostic.open_float)
-  end,
+		keymap.set("n", "K", vim.lsp.buf.hover, {})
+		keymap.set("n", "D", vim.lsp.buf.definition, {})
+		keymap.set({ "n", "v", "i" }, "<C-Enter>", vim.lsp.buf.code_action, {})
+		keymap.set("n", "E", vim.diagnostic.open_float)
+	end,
 }
